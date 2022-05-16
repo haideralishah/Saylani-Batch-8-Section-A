7757ad027d2bd18818467eaaa494d45c29414085
haideralishah <headeralishah@gmail.com>
2022-05-16 10:43:56 +0500

    javascript started var, alert, consolelog, prompt, confirm, string, number, boolean

---
+++ JavaScript/var, alert, consolelog, prompt, confirm, string, number, boolean/app.js
@@ -0,0 +1,86 @@
+// console.log(500);
+
+// console.log("haider 123 *-+87$67#@@    ");
+
+// console.log("03413542800");
+
+
+/*
+string
+numbers (0123456789)
+boolean   (true / false)
+
+
+
+
+
+
+objects
+functions
+
+
+
+*/
+
+// var userName;
+// userName = "Haider Ali";
+// var userName = "Haider Ali";
+// console.log(userName);
+
+// userName = "Akram Hussain";
+// console.log(userName);
+
+
+
+
+
+
+
+// var greetUser = "Hello Mr. 'Haider'";
+// Hello Mr. 'Haider'
+
+// alert(greetUser);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var userName = prompt("what is your name?", "Imran ");
+// console.log(userName);
+
+
+
+
+
+
+
+
+
+
+
+// var isUserReady = confirm("are you older than 15?");
+// console.log(isUserReady);
+
+
+
+
+
+
+var firstName = "Haider";
+var lastName = "Ali";
+var seperator = " ";
+console.log(firstName + seperator + lastName);
+
+
