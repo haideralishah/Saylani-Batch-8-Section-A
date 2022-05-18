a67b1d4ced1c0976ef259060df31f8f42818dfa4
haideralishah <headeralishah@gmail.com>
2022-05-18 10:56:50 +0500

    number and maths expression functions and onclick intro

---
+++ JavaScript/class 1 - var, alert, consolelog, prompt, confirm, string, number, boolean/app.js
@@ -0,0 +1,160 @@
+// console.log(500);
+
+// console.log("haider 123 *-+87$67#@@    ");
+
+// console.log("03413542800");
+
+
+/*
+string //"haider"
+numbers (0123456789)
+boolean   (true / false)
+
+
+
+
+
+objects
+
+
+
+
+
+
+console.log();
+alert();
+prompt();
+confirm();
+
+
+*/
+
+// var userName;
+// userName = "Haider Ali";
+// var userName = "Haider Ali";
+// console.log(userName);
+
+// userName = "Akram Hussain";
+// console.log(userName);
+
+
+
+
+
+
+
+// var greetUser = "Hello Mr. 'Haider'";
+// // Hello     Mr. 'Haider'
+
+// alert(greetUser);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var userName = prompt("what is your name?", "Imran ");
+// console.log(userName);
+
+
+
+
+
+
+
+
+
+
+
+// var isUserReady = confirm("are you older than 15?");
+// console.log(isUserReady);
+
+
+
+
+
+
+// var firstName = "Haider";
+// var lastName = "Ali";
+// var seperator = " ";
+// console.log(firstName + seperator + lastName);
+
+// var num1 = "102";
+// var num2 = "200";
+// var num3 = 100;
+// console.log(num1 - num2 * num2 / num3);
+
+
+
+
+
+
+
+
+// var mathsFee = +prompt("enter Maths fee");
+// var CSFee = +prompt("enter CS fee");
+// var totalFee = mathsFee + CSFee;
+
+// console.log(totalFee);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var htmlMarks = Number(prompt("what is your HTML marks?"));
+// var cssMarks = Number(prompt("what is your CSS marks?"));
+
+// var htmlMarks = +prompt("what is your HTML marks?");
+// var cssMarks = +prompt("what is your CSS marks?");
+
+// var obtainedMarks = htmlMarks + cssMarks;
+// var percentage = obtainedMarks / 200 * 100;
+
+// console.log(percentage + " %");
+
+
+// console.log(Number("55"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
