17f83efd62cf84b720e783663adc512d642a720c
haideralishah <headeralishah@gmail.com>
2022-05-23 10:57:16 +0500

    Basic maths operations.

---
+++ JavaScript/class 3 - Mathmatical operations and Variables/app.js
@@ -0,0 +1,198 @@
+// var num1 = 5;
+// var num2 = 12;
+// console.log(num1);
+
+// num1 = num1 + num2;
+
+// console.log(num1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = 5;
+// var num2 = 12;
+
+// // num1 = num1 + num2;
+
+// num1 += num2;
+// num1 -= num2;
+
+// // num1 = num1 - num2;
+
+// console.log(num1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = 5;
+
+// num1 = num1 + 1;
+// num1 += 1;
+
+// console.log(num1++);
+// console.log(num1);
+
+
+// console.log(num1--); // 5
+// console.log(--num1);  // 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = 5;
+// var num2 = 3;
+
+
+//           5      +  7     +  4     +  4     -   2
+// var sum = num1++ + ++num1 + ++num2 + num2-- - --num2;
+
+// console.log(sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = 5;
+// var num2 = 3;
+
+// //         5    -   4    + 7      +   24
+// //         5     -  4     +  7     +   3    *    8
+// var sum = num1++ - ++num2 + ++num1 + --num2 * ++num1;  // 32
+
+// console.log(sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = 27;
+// var num2 = 3;
+
+// console.log(num1 % num2);
+
+
+
+
+
+// console.log(5 ** 3);
+
+// console.log(5 * 5 * 5);
+
+// var num = 64; // 8 * 8
+
+console.log(Math.sqrt(num));
+
+// console.log(27 ** 0.3333);
+
+
+
+
+
+
