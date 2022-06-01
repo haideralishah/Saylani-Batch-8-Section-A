70dbb4ebb9cb03c4b91941960df7ca4169b162c0
haideralishah <headeralishah@gmail.com>
2022-06-01 11:01:20 +0500

    array almost finished.

---
+++ JavaScript/class 5 - Array/app.js
@@ -0,0 +1,229 @@
+
+
+// var cities = ["karachi", "lahore", "peshawar"];
+
+// console.log(cities[2]);
+
+
+
+
+// var cities = [];
+
+// cities[0] = "Karachi";
+
+// cities[2] = "Lahore";
+
+// console.log(cities[1]);
+
+// cities[1] = "Peshawar";
+
+// console.log(cities);
+
+
+
+
+
+
+
+
+// var fruit1 = prompt('what is your favorite fruit?');
+
+// var fruits = [fruit1];
+
+// var fruits = [];
+
+// fruits[1] = fruit1;
+
+// var fruits = [
+//     prompt('what is your favorite fruit?'),
+//     prompt('what is your favorite fruit?'),
+//     prompt('what is your favorite fruit?')
+// ];
+
+// console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var mixedArr = ["karachi", "lahore", 705955, true];
+// console.log(mixedArr);
+
+
+
+
+
+// var arr = [
+//     ["taha", "zain", "sabeer"],
+//     [2643, 2644, 2477],
+//     [true, false, true]
+// ];
+
+// console.log(arr[0][2]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var fruits = ['apple', 'banana', 'watermelon'];
+
+// fruits.pop();
+// console.log(fruits);
+
+// fruits.push("oranges", "mango", "grapes");
+
+// console.log(fruits);
+
+
+
+
+
+// var fruits = ['apple', 'banana', 'watermelon'];
+
+// fruits.shift();
+
+// console.log(fruits);
+
+// fruits.unshift("Mango", 'grapes');
+
+// console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var students = [];
+// var studentNameInput = document.querySelector("#student-name");
+// var studentsListEl = document.querySelector("#students-list");
+
+// function addStudent() {
+//     students.push(studentNameInput.value);
+//     studentsListEl.innerHTML = students;
+//     studentNameInput.value = "";
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var fruits = ['apple', 'banana', 'orange', 'mango', 'watermelon'];
+
+// fruits.splice(1, 2, 'grapes', 'peach');
+fruits.splice(2, 0, "grapes", "peach");
+
+console.log(fruits);
+
+
