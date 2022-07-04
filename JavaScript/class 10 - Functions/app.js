6bd87a5d0e2c7ad74d9730fdcd6e4a06d6b646be
haideralishah <headeralishah@gmail.com>
2022-07-04 11:00:59 +0500

    date and time finished functions concept

---
+++ JavaScript/class 10 - Functions/app.js
@@ -0,0 +1,197 @@
+
+
+// function prepareMeal(ingrd1, ingrd2) {
+
+//     console.log(ingrd1, ingrd2);
+
+// }
+
+// prepareMeal("Egg", "Paratha");
+
+// prepareMeal("Bengan", "Roti");
+
+// prepareMeal("Karela", "Roti");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var userName = "Haider";
+
+// function greetUser() {
+//     userName = "Ali";
+//     console.log(userName);
+// }
+
+// console.log(userName);
+
+// greetUser();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var sum;
+
+// function calcSum() {
+//     sum = 2 + 2;
+// }
+// calcSum();
+
+// console.log(sum);
+
+// function calcMult() {
+//     sum = sum * 4;
+// }
+
+// calcMult();
+// console.log(sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// sum();
+
+// function sum() {
+//     console.log(2 + 2);
+// }
+
+
+// x = 5;
+
+// console.log(x);
+
+// var x = 7;
+// console.log(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var userName = prompt('what is your name?');
+
+function calculateZakat(nisab) {
+    var zakatRate = 2.5;
+    var zakatAmount = nisab * zakatRate / 100;
+    return zakatAmount;
+}
+
+var farzZakat = calculateZakat(100000);
+
+console.log(farzZakat);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- JavaScript/class 9 - Date & Time/app.js
+++ JavaScript/class 9 - Date & Time/app.js
@@ -102,11 +102,133 @@
 
 
 
-var today = (new Date()).getTime();
-var courseEnd = (new Date("November 4, 2022")).getTime();
-var remainingMSeconds = courseEnd - today;
-var remaingDays = remainingMSeconds / 1000 / 60 / 60 / 24;
-console.log(Math.ceil(remaingDays));
+// var today = (new Date()).getTime();
+// var courseEnd = (new Date("November 4, 2022")).getTime();
+// var remainingMSeconds = courseEnd - today;
+// var remaingDays = remainingMSeconds / 1000 / 60 / 60 / 24;
+// console.log(Math.ceil(remaingDays));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var daysName = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+// var now = new Date();
+
+// console.log(now.getDay());
+// // console.log(daysName[now.getDay()])
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getFullYear());
+// console.log(now.getHours());
+// console.log(now.getMinutes());
+// console.log(now.getSeconds());
+// console.log(now.getMilliseconds());
+// console.log(now.getTime());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var now = new Date();
+// var hours = now.getHours();
+// var min = now.getMinutes();
+
+// var doomsDay = new Date("September 11, 2022");
+
+// var nowInMS = now.getTime();
+// var doomsDayInMS = doomsDay.getTime();
+
+// var remainingTimeInMS = doomsDayInMS - nowInMS;
+
+// var remainingTimeInDays = Math.floor(remainingTimeInMS / 1000 / 60 / 60 / 24);
+// console.log(remainingTimeInDays + " and " + hours + " hours " + min + " minutes.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var now = new Date();
+// var newCentury = new Date("January 1, 2001");
+
+// var nowInMS = now.getTime();
+// var newCenturyInMS = newCentury.getTime();
+// var msPassed = nowInMS - newCenturyInMS;
+// console.log(Math.floor(msPassed / 1000 / 60 / 60));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
