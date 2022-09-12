a075af539879421826ede3890f95f2e0387bda8a
haideralishah <headeralishah@gmail.com>
2022-09-12 08:56:40 +0500

    ES 6 started

--- JavaScript/Revision class DOM (Todo)/app.js
+++ JavaScript/Revision class DOM (Todo)/app.js
@@ -1,16 +1,17 @@
 var newTodo = document.querySelector("#new-todo");
 var todoList = document.querySelector(".list-item");
-
+var addBtn = document.querySelector("#add-todo-btn");
 function addNewTodo() {
     var newTodoItem = newTodo.value;
-    var li = newElements("LI", newTodoItem);
+    var li = newElements("LI", newTodoItem); // <li>take breakfast</li>
 
-    var edt = newElements("BUTTON", "Edit");
-    li.appendChild(edt);
+    var edt = newElements("BUTTON", "Edit"); // <button>Edit</button>
+    edt.setAttribute("onclick", "editItem(this)"); //<button onclick="editItem(this)">Edit</button>
+    li.appendChild(edt);                    // <li>take breakfast<button>Edit</button></li>
 
-    var del = newElements("BUTTON", "Delete");
-    del.setAttribute("onclick", "deleteTodo(this)");
-    li.appendChild(del);
+    var del = newElements("BUTTON", "Delete"); // <button>Delete</button>
+    del.setAttribute("onclick", "deleteTodo(this)");// <button onclick="deleteTodo(this)">Delete</button>
+    li.appendChild(del);                    //<li>take breakfast<button>Edit</button><button onclick="deleteTodo(this)">Delete</button></li>
 
     todoList.appendChild(li);
     newTodo.value = "";
@@ -25,3 +26,109 @@ function newElements(elementName, text) {
 function deleteTodo(element) {
     todoList.removeChild(element.parentNode);
 }
+
+//parent.removeChild(childRefrence)
+
+
+
+function deleteAll() {
+    todoList.innerHTML = "";
+}
+
+
+var editItemRef;
+function editItem(element) {
+    editItemRef = element.parentNode;
+    newTodo.value = editItemRef.firstChild.nodeValue;
+
+    addBtn.innerHTML = "Save";
+    addBtn.setAttribute("onclick", 'saveTodo()');
+    // addBtn.onclick = saveTodo;
+}
+
+function saveTodo() {
+    var updatedTodo = newTodo.value;
+    editItemRef.firstChild.nodeValue = updatedTodo;
+
+    newTodo.value = "";
+    addBtn.innerHTML = "Add";
+    addBtn.setAttribute("onclick", 'addNewTodo()');
+    editItemRef = undefined;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JavaScript/class 16 - ES6/app.js
@@ -0,0 +1,161 @@
+// var userName = "hadier";
+
+// let userName = prompt('what is your name?');
+// console.log(userName)
+
+
+
+
+// var userName = 'haider';
+// function greetUser(name) {
+//     if (isNaN(name)) {
+//         let userName = prompt('what is your nick name');
+//         alert("Hey! " + userName);
+//     }
+//     console.log(userName);
+// }
+// greetUser("ali");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const url = "http://www.google.com";
+// url = "haider";
+// console.log(url);
+
+
+
+
+
+
+
+// let para = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus asperiores rem aut ea earum natus ad
+// excepturi,
+
+
+// cumque quisquam doloribus accusamus molestiae facere atque delectus recusandae maiores error
+// temporibus adipisci.`
+
+// console.log(para);
+
+
+
+// let firstName = `haider`;
+// let lastName = `ali`;
+// let greeting = `<h1>Good morning ${firstName} ${lastName}</h1>`;
+// console.log(greeting);
+
+// let greeting = "Good morning " + fullName;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(`49 ${7 + 5}`);
+
+// console.log(`${4 + 6}`);
+
+
+
+
+
+
+
+
+let user = {
+    name: 'haider',
+    rollNumber: 2643,
+    subject: 'JavaScript'
+}
+
+let { subject } = user;
+console.log(subject);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
