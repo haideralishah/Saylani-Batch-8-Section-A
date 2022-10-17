13473503050b59f3ed39e2318a6e5a6b6dc12ccc
haideralishah <headeralishah@gmail.com>
2022-10-17 00:38:11 -0400

    firestorage image upload and downloadable URL get.

--- JavaScript/class 17 - Firebase/app.js
+++ JavaScript/class 17 - Firebase/app.js
@@ -1,7 +1,7 @@
-import { auth, db } from "./firebaseconfig.js";
+import { auth, db, storage } from "./firebaseconfig.js";
 import { createUserWithEmailAndPassword, sendEmailVerification } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
-import { doc, setDoc, deleteDoc, collection, where, query, getDocs } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
-
+import { doc, setDoc, deleteDoc, collection, where, query, getDocs, orderBy, limit, startAt, startAfter } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
+import { ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-storage.js";
 
 // let email = document.querySelector("#email");
 // let password = document.querySelector("#password");
@@ -20,7 +20,8 @@ import { doc, setDoc, deleteDoc, collection, where, query, getDocs } from "https
 //         await setDoc(userDocRef, {
 //             username: username.value,
 //             rollNum: rollNum.value,
-//             email: email.value
+//             email: email.value,
+//              uid:user.user.uid
 //         });
 
 //         verifyEmail()
@@ -103,90 +104,124 @@ import { doc, setDoc, deleteDoc, collection, where, query, getDocs } from "https
 
 
 
-const citiesRef = collection(db, "cities");
+// const citiesRef = collection(db, "cities");
 
-await setDoc(doc(citiesRef, "SF"), {
-    name: "Karachi",
-    state: "Sindh",
-    country: "Pakistan",
-    capital: false,
-    population: 860000,
-    regions: ["west_coast", "norcal"]
-});
+// await setDoc(doc(citiesRef, "SF"), {
+//     name: "Karachi",
+//     state: "Sindh",
+//     country: "Pakistan",
+//     capital: false,
+//     population: 860000,
+//     regions: ["west_coast", "norcal"]
+// });
 
-await setDoc(doc(citiesRef, "ABC"), {
-    name: "Hyderabad",
-    state: "Sindh",
-    country: "Pakistan",
-    capital: false,
-    population: 860000,
-    regions: ["west_coast", "norcal"]
-});
-await setDoc(doc(citiesRef, "LA"), {
-    name: "Los Angeles", state: "CA", country: "USA",
-    capital: false, population: 3900000,
-    regions: ["west_coast", "socal"]
-});
-await setDoc(doc(citiesRef, "DC"), {
-    name: "Washington, D.C.", state: null, country: "USA",
-    capital: true, population: 680000,
-    regions: ["east_coast"]
-});
-await setDoc(doc(citiesRef, "TOK"), {
-    name: "Tokyo", state: null, country: "Japan",
-    capital: true, population: 9000000,
-    regions: ["kanto", "honshu"]
-});
-await setDoc(doc(citiesRef, "BJ"), {
-    name: "Beijing", state: null, country: "China",
-    capital: true, population: 21500000,
-    regions: ["jingjinji", "hebei"]
-});
-
-
+// await setDoc(doc(citiesRef, "ABC1"), {
+//     name: "Hyderabad",
+//     state: "Sindh",
+//     country: "Pakistan",
+//     capital: false,
+//     population: 860000,
+//     regions: ["west_coast", "norcal"]
+// });
+// await setDoc(doc(citiesRef, "LA1"), {
+//     name: "Los Angeles", state: "CA", country: "USA",
+//     capital: false, population: 3900000,
+//     regions: ["west_coast", "socal"]
+// });
+// await setDoc(doc(citiesRef, "DC1"), {
+//     name: "Washington, D.C.", state: null, country: "USA",
+//     capital: true, population: 680000,
+//     regions: ["east_coast"]
+// });
+// await setDoc(doc(citiesRef, "TOK1"), {
+//     name: "Tokyo", state: null, country: "Japan",
+//     capital: true, population: 9000000,
+//     regions: ["kanto", "honshu"]
+// });
+// await setDoc(doc(citiesRef, "BJ1"), {
+//     name: "Beijing", state: null, country: "China",
+//     capital: true, population: 21500000,
+//     regions: ["jingjinji", "hebei"]
+// });
 
 
+// await setDoc(doc(citiesRef, "SF1"), {
+//     name: "Karachi",
+//     state: "Sindh",
+//     country: "Pakistan",
+//     capital: false,
+//     population: 860000,
+//     regions: ["west_coast", "norcal"]
+// });
 
-
-
-
-
-
-
-
-
-
-let collectionRef = collection(db, "cities");
-
-// let condition = where("state", "==", "CA");
-// let condition = where("capital", "==", true);
-// let condition = where("population", "<", 1000000);
-// let condition = where("capital", "!=", false)
-
-// if(regions.includes("west-coast"))
-// let condition = where("regions", "array-contains", "west_coast");
-
-// if('country' === "USA" || "country" === "Japan" || "country" === "Pakistan")
-// let condition = where('country', 'in', ['USA', 'Japan', 'Pakistan']);
-
-// let condition = where('country', 'not-in', ['USA', 'Japan'])
-// let condition =  where('regions', 'array-contains-any', ['west_coast', 'east_coast'])
-
-let condition1 = where("state", "==", "Sindh");
-let condition2 = where("name", "==", "Karachi");
-
-// const q = query(collectionRef, condition);
-const q = query(collectionRef, condition1, condition2);
-
-let citiesSnapshot = await getDocs(q);
-// console.log(citiesSnapshot);
-
-citiesSnapshot.forEach((doc) => {
-    console.log(doc.id, " => ", doc.data());
-});
+// await setDoc(doc(citiesRef, "ABC2"), {
+//     name: "Hyderabad",
+//     state: "Sindh",
+//     country: "Pakistan",
+//     capital: false,
+//     population: 860000,
+//     regions: ["west_coast", "norcal"]
+// });
+// await setDoc(doc(citiesRef, "LA2"), {
+//     name: "Los Angeles", state: "CA", country: "USA",
+//     capital: false, population: 3900000,
+//     regions: ["west_coast", "socal"]
+// });
+// await setDoc(doc(citiesRef, "DC2"), {
+//     name: "Washington, D.C.", state: null, country: "USA",
+//     capital: true, population: 680000,
+//     regions: ["east_coast"]
+// });
+// await setDoc(doc(citiesRef, "TOK2"), {
+//     name: "Tokyo", state: null, country: "Japan",
+//     capital: true, population: 9000000,
+//     regions: ["kanto", "honshu"]
+// });
+// await setDoc(doc(citiesRef, "BJ2"), {
+//     name: "Beijing", state: null, country: "China",
+//     capital: true, population: 21500000,
+//     regions: ["jingjinji", "hebei"]
+// });
 
 
 
+// await setDoc(doc(citiesRef, "SF2"), {
+//     name: "Karachi",
+//     state: "Sindh",
+//     country: "Pakistan",
+//     capital: false,
+//     population: 860000,
+//     regions: ["west_coast", "norcal"]
+// });
+
+// await setDoc(doc(citiesRef, "ABC3"), {
+//     name: "Hyderabad",
+//     state: "Sindh",
+//     country: "Pakistan",
+//     capital: false,
+//     population: 860000,
+//     regions: ["west_coast", "norcal"]
+// });
+// await setDoc(doc(citiesRef, "LA3"), {
+//     name: "Los Angeles", state: "CA", country: "USA",
+//     capital: false, population: 3900000,
+//     regions: ["west_coast", "socal"]
+// });
+// await setDoc(doc(citiesRef, "DC3"), {
+//     name: "Washington, D.C.", state: null, country: "USA",
+//     capital: true, population: 680000,
+//     regions: ["east_coast"]
+// });
+// await setDoc(doc(citiesRef, "TOK3"), {
+//     name: "Tokyo", state: null, country: "Japan",
+//     capital: true, population: 9000000,
+//     regions: ["kanto", "honshu"]
+// });
+// await setDoc(doc(citiesRef, "BJ3"), {
+//     name: "Beijing", state: null, country: "China",
+//     capital: true, population: 21500000,
+//     regions: ["jingjinji", "hebei"]
+// });
 
 
 
@@ -202,3 +237,76 @@ citiesSnapshot.forEach((doc) => {
 
 
 
+
+// let collectionRef = collection(db, "cities");
+
+// // let condition = where("state", "==", "CA");
+// // let condition = where("capital", "==", true);
+// // let condition = where("population", "<", 1000000);
+// // let condition = where("capital", "!=", false)
+
+// // if(regions.includes("west-coast"))
+// // let condition = where("regions", "array-contains", "west_coast");
+
+// // if('country' === "USA" || "country" === "Japan" || "country" === "Pakistan")
+// // let condition = where('country', 'in', ['USA', 'Japan', 'Pakistan']);
+
+// // let condition = where('country', 'not-in', ['USA', 'Japan'])
+// // let condition =  where('regions', 'array-contains-any', ['west_coast', 'east_coast'])
+
+// // let condition1 = where("state", "==", "Sindh");
+// // let condition2 = where("", "==", "Karachi");
+
+// // const q = query(collectionRef, condition);
+
+// // const q = query(collectionRef, condition1, condition2);
+
+// // let condition = where("state", "==", "CA");
+// let sort = orderBy("population");
+// let lim = limit(3);
+// let start = startAt(1000000);
+// const q = query(collectionRef, sort, start);
+
+// let citiesSnapshot = await getDocs(q);
+// // console.log(citiesSnapshot);
+
+// citiesSnapshot.forEach((doc) => {
+//     console.log(doc.id, " => ", doc.data().name);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let uploadInputEl = document.querySelector("#profile-pic");
+let btn = document.querySelector("#upload-pic");
+btn.addEventListener("click", uploadPic);
+
+async function uploadPic() {
+    let file = uploadInputEl.files[0];
+    let imageRef = ref(storage, `images/${file.name}`);
+    try {
+        let uploaded = await uploadBytes(imageRef, file);
+        let url = await getDownloadURL(imageRef);
+        console.log(url, 'downloadable URL');
+    } catch (e) {
+        console.log(e);
+    }
+    // console.log("upload called", uploadInputEl.files[0]);
+}
--- JavaScript/class 17 - Firebase/firebaseconfig.js
+++ JavaScript/class 17 - Firebase/firebaseconfig.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
 import { getAuth , onAuthStateChanged} from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-storage.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyD_H6VAD2ttyuZCimFMwW2Bm2u95ucKOeE",
@@ -16,7 +17,7 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
+export const storage = getStorage(app);
 
 
 
